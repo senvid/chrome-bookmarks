5f397df77d33130791db4039044f78b46ecbe1b7
senvid <ssww1029@@gmail.com>
2015-07-19 22:51:36 +0800

    complement done

--- book.js
+++ book.js
@@ -25,6 +25,81 @@
 //     }
 // }
 
+
+//prototype method unique
+//use: myArray.unique()
+Array.prototype.unique = function() {
+    var hash = {};
+    var r = [];
+    var val;
+    var type;
+    for (var i = 0; i < this.length; i++) {
+        val = this[i];
+        type = typeof val;
+        if (!hash[val]) {
+            hash[val] = [type];
+            r.push(val);
+        } else if (hash[val].indexOf(type) < 0) {
+            hash[val].push(type);
+            r.push(val);
+        }
+    }
+    return r;
+}
+
+//union no repeat
+//use: l = myArray.union(a)
+Array.prototype.union = function(a) {
+    return this.concat(a).unique();
+}
+
+//get intersect
+//use: l = a.intersect(b)
+Array.prototype.intersect = function(a) {
+    //hash:{val:{num:1,str:2},...}
+    var hash = {};
+    var r = [];
+    var type;
+    var val;
+    for (var i = 0; i < a.length; i++) {
+        //h:{number:1,string:2,...}
+        var h = {};
+        val = a[i];
+        type = typeof val;
+        if (!hash[val]) {
+            h[type] = 1;
+            hash[val] = h;
+        } else {
+            if (!hash[val][type]) {
+                hash[val][type] = 1;
+            } else {
+                hash[val][type]++;
+            };
+        };
+    };
+    for (var i = 0; i < this.length; i++) {
+        type = typeof this[i];
+        if (hash[this[i]] && hash[this[i]][type] > 0) {
+            hash[this[i]][type]--;
+            r.push(this[i]);
+        };
+    };
+    return r;
+}
+
+// get complement
+//use: l=a.complement(b)
+Array.prototype.complement = function(a) {
+    for (var i = 0; i < this.length; i++) {
+        var n = a.indexOf(this[i]);
+        if (n != -1) {
+            a.splice(n,1);
+        };
+    };
+    return a;
+}
+
+//get tree
 function parseTree() {
     var tree = chrome.bookmarks.getTree(
         function(tree) {
@@ -89,67 +164,6 @@ function getList() {
     };
 };
 
-//prototype method unique
-//use: myArray.unique()
-Array.prototype.unique = function() {
-    var hash = {};
-    var r = [];
-    var val;
-    var type;
-    for (var i = 0; i < this.length; i++) {
-        val = this[i];
-        type = typeof val;
-        if (!hash[val]) {
-            hash[val] = [type];
-            r.push(val);
-        } else if (hash[val].indexOf(type) < 0) {
-            hash[val].push(type);
-            r.push(val);
-        }
-    }
-    return r;
-}
-
-//union no repeat
-//use: l = Array.union(a,b)
-Array.union = function(a, b) {
-    return a.concat(b).unique();
-}
-
-//get intersect
-//use l = a.intersect(b)
-Array.prototype.intersect = function(a) {
-    //{val:{num:1,str:2},...}
-    var hash = {};
-    var r = [];
-    var type;
-    var val;
-
-    for (var i = 0; i < a.length; i++) {
-        //{num:1,str:2}
-        var h = {};
-        val = a[i];
-        type = typeof val;
-        if (!hash[val]) {
-            h[type] = 1;
-            hash[val] = h;
-        } else {
-            if (!hash[val][type]) {
-                hash[val][type] = 1;
-            } else {
-                hash[val][type]++;
-            };
-        };
-    };
-    for (var i = 0; i < this.length; i++) {
-        type = typeof this[i];
-        if (hash[this[i]] && hash[this[i]][type] > 0) {
-            hash[this[i]][type]--;
-            r.push(this[i]);
-        };
-    };
-    return r;
-}
 
 //different set of the two list
 function getDiff(a, b) {
@@ -167,7 +181,7 @@ function compareJson(a) {
                 //aList.push(a.children[i].id);
                 if (a.children[i].url != undefined) {
                     compareJson(a.children[i]);
-                } else {
+                 } else {
                     aList.push([a.children[i].id, a.children[i].index, a.children[i].parentId, a.children[i].title]);
                     compareJson(a.children[i]);
                 };
@@ -189,7 +203,7 @@ document.addEventListener('DOMContentLoaded', function() {
     console.log("start..");
     //onload();
 
-    //getList();
+    getList();
     var c1 = [1, 5, 5, 5, 9, "5", "5", "", "1", "7"];
     var c2 = [1, 5, 5, 30, "5", ""];
     var c3 = [1, 5, ""];
@@ -197,7 +211,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //console.log(c1.unique());
     //console.log(c2.unique());
     //console.log(c1.unique().intersect(c2.unique()));
-    console.log(c4.intersect(c3));
+   // console.log(c4.intersect(c3));
     // document.addEventListener("somethingAPI", function() {
     //     onload();
     // });
